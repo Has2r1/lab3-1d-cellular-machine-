32a1933f70a5eac72ed58cdf48a627d8fea457fe
Has2r <Has2r@DESKTOP-6Q87078>
2024-03-08 22:44:47 +0700

    Добавьте файлы проекта.

---
+++ lab3 (1d cellular machine)/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace lab3__1d_cellular_machine_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rule = new System.Windows.Forms.TextBox();
+            this.numberOfLines = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1020, 1020);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1076, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Запустить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rule
+            // 
+            this.rule.Location = new System.Drawing.Point(1301, 99);
+            this.rule.Name = "rule";
+            this.rule.Size = new System.Drawing.Size(100, 20);
+            this.rule.TabIndex = 2;
+            // 
+            // numberOfLines
+            // 
+            this.numberOfLines.Location = new System.Drawing.Point(1301, 140);
+            this.numberOfLines.Name = "numberOfLines";
+            this.numberOfLines.Size = new System.Drawing.Size(100, 20);
+            this.numberOfLines.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(1765, 1061);
+            this.Controls.Add(this.numberOfLines);
+            this.Controls.Add(this.rule);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox rule;
+        private System.Windows.Forms.TextBox numberOfLines;
+    }
+}
+
---
+++ lab3 (1d cellular machine)/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab3__1d_cellular_machine_
+{
+    
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private Dictionary<(bool, bool, bool), bool> currentRule = new Dictionary<(bool, bool, bool), bool>();
+        private int lines = 100;
+        private int Ypos = 0;
+        private int Xpos = 0;
+        private List<List<bool>> data = new List<List<bool>>();
+        private readonly Brush whiteBrush = new SolidBrush(Color.White);
+        private readonly Brush blackBrush = new SolidBrush(Color.Black);
+        private Graphics g;
+        private List<bool> genZero = new List<bool>();
+        private readonly List<int> rules = new List<int>()
+        {
+            30, 90, 184,
+            150, 105, 101, 73, 
+            54, 60, 137, 
+            110
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            g = panel1.CreateGraphics();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int.TryParse(rule.Text, out ruleNumber);
+            int.TryParse(numberOfLines.Text, out lines);
+
+            
+
+            foreach (int rule in rules)
+            {
+                button1.Text = "новая";//rule.ToString();
+                var binary = Convert.ToString(rule, 2).PadLeft(8, '0').ToCharArray();
+                currentRule.Clear();
+                currentRule.Add((false, false, false), binary[0] == '1');
+                currentRule.Add((false, false, true), binary[1] == '1');
+                currentRule.Add((false, true, false), binary[2] == '1');
+                currentRule.Add((false, true, true), binary[3] == '1');
+                currentRule.Add((true, false, false), binary[4] == '1');
+                currentRule.Add((true, false, true), binary[5] == '1');
+                currentRule.Add((true, true, false), binary[6] == '1');
+                currentRule.Add((true, true, true), binary[7] == '1');
+
+                genZero.Clear();
+                for (int i = 0; i < lines; i++)
+                {
+                    genZero.Add(random.NextDouble() >= 0.1);
+                }
+
+                for (int k = 0; k < 40; k++)
+                {
+                    data.Clear();
+                    data.Add(new List<bool>(genZero));
+                    for (int i = 1; i < lines; i++)
+                    {
+                        data.Add(new List<bool>());
+                        for (int j = 0; j < lines; j++)
+                        {
+                            data[i].Add(currentRule[(data[i - 1][(j - 1 + lines) % lines], data[i - 1][j], data[i - 1][(j + 1) % lines])]);
+                        }
+                    }
+                    genZero = new List<bool>(data[lines - 1]);
+
+                    for (int i = 0; i < lines; i++)
+                    {
+                        Ypos = 10 + i * 10;
+                        for (int j = 0; j < lines; j++)
+                        {
+                            Xpos = 10 + j * 10;
+                            if (data[i][j])
+                            {
+                                g.FillRectangle(whiteBrush, Xpos, Ypos, 10, 10);
+                            }
+                            else
+                            {
+                                g.FillRectangle(blackBrush, Xpos, Ypos, 10, 10);
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    }
+}
